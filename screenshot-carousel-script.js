417644a8eb2bb0a1d13257ebc183e256518eeabc
Everest799 <krishlamsal@hotmail.com>
2026-04-03 19:22:47 +0100

    Add carousel dots
    Add a line of dots representing which image of the carousel the user is
    on.

--- screenshot-carousel-script.js
+++ screenshot-carousel-script.js
@@ -1,5 +1,20 @@
 let currentImage = 0;
-const totalImages = document.querySelectorAll(".app-screenshot").length;
+const screenshots = document.querySelectorAll(".app-screenshot");
+const totalImages = screenshots.length;
+const dotsContainer = document.querySelector(".carousel-dots");
+
+// 1. Create dots
+for (let i = 0; i < totalImages; i++) {
+  const dot = document.createElement("div");
+  dot.classList.add("carousel-dot");
+  dot.addEventListener("click", () => {
+    currentImage = i;
+    updateCarousel();
+  });
+  dotsContainer.appendChild(dot);
+}
+
+const dots = dotsContainer.querySelectorAll(".carousel-dot");
 
 function nextImage() {
   currentImage = (currentImage + 1) % totalImages;
@@ -14,6 +29,10 @@ function previousImage() {
 function updateCarousel() {
   const translate = -currentImage * 312;
   document.documentElement.style.setProperty("--translate-x", `${translate}px`);
+  // 2. Update active dot
+  dots.forEach((dot, idx) => {
+    dot.classList.toggle("active", idx === currentImage);
+  });
 }
 
 updateCarousel();
